f80c45c3c391a88a46af2b7140a6ba383252285f
Marnu L <web-chair@na.org.za>
2013-02-12 19:12:41 +0200

    Initial commit
    Syncing the boilerplate

---
+++ index.php
@@ -0,0 +1,79 @@
+<!doctype html>
+
+<!--[if lt IE 7]> <html class="lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>    <html class="lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>    <html class="lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+
+<head>
+	<meta charset="utf-8" />
+	<meta name="author" content="***" />
+	<meta name="copyright" content="***" />
+	<meta name="description" content="***" />
+	<meta name="keywords" content="***" />
+
+	<title>Front-end Web Dev | Marnu Lombard | Portfolio</title>
+
+	<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<script src="js/selectivizr.min.js"></script>
+	<![endif]-->
+
+	<!-- Mobile Specific Metas -->
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+	<!-- CSS -->
+	<link rel="stylesheet" href="css/style.css" />
+
+	<!-- Favicons -->
+	<!-- No favicon for now -->
+
+
+</head>
+<body>
+
+	<noscript>
+	</noscript>
+
+	<div class="grid-wrapper main">
+
+		<header class="one-whole">
+			<nav>
+				<ul class="nav">
+					<li><a href=""></a></li>
+					<li><a href=""></a></li>
+					<li><a href=""></a></li>
+					<li><a href=""></a></li>
+					<li><a href=""></a></li>
+					<li><a href=""></a></li>
+				</ul>
+			</nav>
+		</header><!-- // header -->
+		
+		<section class="three-quarters center primary">
+		</section>
+		
+	</div><!-- //main grid-wrapper -->
+
+
+	<footer>
+	</footer>
+
+<!--------------------
+		Scripts
+-------------------->
+
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
+	<script>
+		window.jQuery || document.write('<script src="js/jquery-1.8.1.min.js"><\/script>')
+	</script>
+	
+	
+	<!-- swap out pngs if svg is not supported - /gsdesign.com/blog/practical-svg-usage/ -->
+	<script src="js/modernizr.svg.min.js"></script>
+	<script>
+		$(document).ready(function() {if (Modernizr.inlinesvg) {$('.svg').each(function() {var $this = $(this);var src = $this.attr("src").replace(".png", ".svg");$this.attr("src", src); }); } });
+	</script>
+
+</body>
+</html>
